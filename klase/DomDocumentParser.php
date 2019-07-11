8c003346d3bbe0e78461c80a46799a0fac00f545
daliborb20@gmail.com <daliborb20@gmail.com>
2019-07-11 18:24:00 +0200

    drugo

--- klase/DomDocumentParser.php
+++ klase/DomDocumentParser.php
@@ -2,16 +2,22 @@
 class DomDocumentParser
 {
     private $dok;
-    public function __construct ($url)
+    public function __construct($url)//public -> moze se pozvati van klase
     {
-        $opcije = array("http" => array('method' => "GET", 'header' => "User-Agent: doodlBot/0.1\n")
-        );
-        $kontext = stream_context_create($opcije);
-        $dok = new DomDocument();
-        @$dok ->loadHTML(file_get_contents($url, false, $kontext));
-    } 
-}
+     
+        $opcije = [
+            'http' => [
+                'method'=>'GET', 'header' => 'User-Agent: doodleBot/0.1\n'
+            ]
+        ];
 
+        $kontekst = stream_context_create($opcije);
+        $this -> $dok = new DomDocument();
+        @$this->$dok ->loadHTML(file_get_contents($url, false, $kontekst));//supersing errors:D
+
+    }
+
+}
 
 
 ?>
--- pauk.php
+++ pauk.php
@@ -1,10 +1,17 @@
 <?php
 include("klase/DomDocumentParser.php");
+
+
+
+
 function pratiLinkove($url)
 {
     $parser = new DomDocumentParser($url);
 
 }
+
+
+
 $pocetnUrl = "https://www.blic.rs/";
 pratiLinkove($pocetnUrl);
 
